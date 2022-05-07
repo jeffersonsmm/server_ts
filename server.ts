4ad849836a4e3c5cd7b7a2feb67424b300841f5b
Jeff_smm <37228971+jeffersonsmm@users.noreply.github.com>
2022-05-07 20:35:40 -0300

    Delete server.ts

--- server.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import express from 'express'
-
-const app = express()
-
-app.listen(process.env.PORT || 3333, () => {
-  console.log(`✔ server is running on port ${process.env.PORT || 3333}!`);
-})
