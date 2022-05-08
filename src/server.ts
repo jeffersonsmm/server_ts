ccffdc57a16f2e399daaf3a2cb73cc0d5d835a83
Jeff_smm <37228971+jeffersonsmm@users.noreply.github.com>
2022-05-07 21:03:01 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  return res.send('')
+})
+
 app.listen(process.env.PORT || 3333, () => {
   console.log(`✔ server is running on port ${process.env.PORT || 3333}!`);
 })
